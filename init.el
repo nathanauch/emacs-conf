b9b22103ffaeb1aa886259dcca280270c6571f21
Nathan Auch <nathan.auch@gmail.com>
2013-02-19 21:13:23 -0500

    Support local system configuration

--- init.el
+++ init.el
@@ -3,3 +3,5 @@
 ;; load everything in the common directory
 (mapcar 'load-file (directory-files "~/.emacs.d/common" t "\.el$"))
 (mapcar 'load-file (directory-files "~/.emacs.d/ext" t "\.el$"))
+(when (file-exists-p "~/.emacs.d/local")
+  (mapcar 'load-file (directory-files "~/.emacs.d/local" t "\.el$")))
