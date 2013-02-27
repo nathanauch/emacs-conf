315090c3a99b81d332ed6a3924b93a5f476c1538
Nathan Auch <nauch@rim.com>
2013-02-27 12:13:02 -0500

    Fix frame font for Windows vs Mac OS X. Hide menu bars

--- common/appearance.el
+++ common/appearance.el
@@ -1,8 +1,12 @@
 (load-theme 'zenburn t)
 
-(set-frame-font "Menlo-11")
+(set-frame-font
+ (cond ((eq system-type 'windows-nt) "Consolas-11")
+       ((eq system-type 'darwin) "Menlo-11")))
+
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(menu-bar-mode -1)
 (set-variable 'inhibit-startup-screen t)
 (setq ns-pop-up-frames 'nil)
 (toggle-truncate-lines 1)
